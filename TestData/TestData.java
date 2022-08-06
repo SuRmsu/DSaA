021da7b7f1fe6c4ce95c852311ce72e4e6ba723f
SuRmsu <surmsu001@gmail.com>
2022-08-06 10:16:09 +0800

    更改了测试的数据类

--- TestData/TestData.java
+++ TestData/TestData.java
@@ -2,8 +2,13 @@ package TestData;
 
 public  class TestData {
 
-    public static int[] testDataInt = new int[] {3,2,5,6,8,3,4,3,23,54,6,2,2,5,6,3,7,3,65,6,76,8,7898,454,754,35,54,3,576,346,45,34,32,4,12,3,0,8,55,-2,3,45,-43};
-    public char[] testDataChar = new char[] {'a','1','b','c','d','s'};
-    public String[] testDataString = new String[] {"BOE","TENCENT","MICROSOFT","APPLE","GOOGLE","ALIBABA","ZOOM","NIO"};
-    public double[] testDataDouble = new double[] {3.2123,35.554,-25.3443,};
+    public static int[] testDataInt = new int[] {   3,2,5,6,8,3,4,3,2-4,3,54,6,2,2,5,6,3,7,12,-356,
+                                                    3,-23,65,6,76,8,7898,454,754,35,54,3,576,346,
+                                                    45,34,32,4,12,3,0,8,55,-2,3,45,-43,21,50,
+                                                    234,2,91,-3,-12,10};
+    public char[] testDataChar = new char[] {'a','1','b','c','d','s','9','S',',','.',';','`','S','2'};
+    public String[] testDataString = new String[] { "BOE","TENCENT","MICROSOFT","APPLE","GOOGLE","ALIBABA",
+                                                    "ZOOM","NIO","XIAOMI","REDMI","ORACLE"};
+    public double[] testDataDouble = new double[] { 3.2123,35.554,-25.3443,23.4,233.21,3.1,3.12,
+                                                    3.13,3.11};
 }
